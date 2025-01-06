2dc35acb2a6d92f48161f5d954c1a0dbecc95a8b
ameentm igoraza <igorazadeveloper@gmail.com>
2025-01-06 19:31:37 +0530

    commiting new code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HeroSection from "./components/heroSection/HeroSection";
+import NavBar from "./components/navBar/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <HeroSection />
+    </>
+  );
+}
+
+export default App;
